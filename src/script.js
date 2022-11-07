1e43e0156852f52f7c846da6037ecc398db198e4
Nufko3 <kollarikmatko@gmail.com>
2022-11-07 19:36:44 +0100

    Menu and scrolling working, sections are wrong

--- src/script.js
+++ src/script.js
@@ -7,3 +7,22 @@ function activeLink()
 }
 
 list.forEach((item) => item.addEventListener("click", activeLink));
+
+$(document).ready(function()
+{
+  $('a').on("click", function(event)
+  {
+    if (this.hash !== "")
+    {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
