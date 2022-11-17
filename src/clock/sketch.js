0d5a3c40104939a4ee3da3c3d47de17cc162e1a5
Nufko3 <kollarikmatko@gmail.com>
2022-11-17 18:16:42 +0100

    Added digital clock

--- src/clock/sketch.js
+++ src/clock/sketch.js
@@ -19,22 +19,23 @@ function drawClock()
   translate(200, 200)
   rotate(-90)
 
-  let h = hour()
-  let min = minute()
-  let s = second()
+  var h = hour()
+  var min = minute()
+  var s = second()
 
-  strokeWeight(8)
-  stroke(255, 0, 75)
   noFill()
-  let secondAngle = map(s, 0, 60, 0, 360)
+  strokeWeight(8)
+  
+  stroke(255, 0, 75)
+  var secondAngle = map(s, 0, 60, 0, 360)
   arc(0, 0, 300, 300, 0, secondAngle)
 
   stroke(150, 100, 255)
-  let minuteAngle = map(min, 0, 60, 0, 360)
+  var minuteAngle = map(min, 0, 60, 0, 360)
   arc(0, 0, 280, 280, 0, minuteAngle)
 
   stroke(100, 255, 20)
-  let hourAngle = map(h % 12, 0, 12, 0, 360)
+  var hourAngle = map(h % 12, 0, 12, 0, 360)
   arc(0, 0, 260, 260, 0, hourAngle)
 
   push()
@@ -58,4 +59,11 @@ function drawClock()
   stroke(255)
   point(0, 0)
   pop()
+
+  fill(255)
+  noStroke()
+  textAlign(CENTER)
+  textStyle(BOLD)
+  textSize(20)
+  text(h + ':' + min + ':' + s, width / 2, 375)
 }
